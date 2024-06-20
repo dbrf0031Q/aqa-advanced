cc3f1c98a394419fc0389fc030381c3284d9ef91
Vika <146200003+dbrf0031Q@users.noreply.github.com>
2024-06-20 16:20:26 +0200

    homework functions

---
+++ Homework/functions.js
@@ -0,0 +1,19 @@
+function calculateArea(width, height) {
+    return width * height;
+  }
+  
+  const calculateAreaExpression = function(width, height) {
+    return width * height;
+  };
+  
+  const calculateAreaArrow = (width, height) => {
+    return width * height;
+  };
+  
+  const area1 = calculateArea(5, 10);
+  const area2 = calculateAreaExpression(3788, 7.366);
+  const area3 = calculateAreaArrow(5-3, 10/2);
+  
+  console.log("Площа прямокутника =", area1);
+  console.log("Площа прямокутника=", area2);
+  console.log("Площа прямокутника =", area3);
