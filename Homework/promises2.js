afe792cdc573e6f0e63eb7e86403def321986769
Vika <146200003+dbrf0031Q@users.noreply.github.com>
2024-07-06 14:43:40 +0200

    homework; promises
    homework; promises

---
+++ Homework/promises2.js
@@ -0,0 +1,27 @@
+function fetchTodo() {
+    return fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error('Error fetching todo:', error));
+}
+
+function fetchUser() {
+    return fetch('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error('Error fetching user:', error));
+}
+
+Promise.all([fetchTodo(), fetchUser()])
+    .then(results => {
+        const [todo, user] = results;
+        console.log('Todo:', todo);
+        console.log('User:', user);
+    })
+    .catch(error => console.error('Error with Promise.all:', error));
+
+Promise.race([fetchTodo(), fetchUser()])
+    .then(result => {
+        console.log('First resolved promise:', result);
+    })
+    .catch(error => console.error('Error with Promise.race:', error));
---
+++ Homework/promises3.js
@@ -0,0 +1,41 @@
+async function fetchTodo() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching todo:', error);
+    }
+}
+
+async function fetchUser() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching user:', error);
+    }
+}
+
+async function fetchDataAll() {
+    try {
+        const results = await Promise.all([fetchTodo(), fetchUser()]);
+        const [todo, user] = results;
+        console.log('Todo:', todo);
+        console.log('User:', user);
+    } catch (error) {
+        console.error('Error with Promise.all:', error);
+    }
+}
+
+async function fetchDataRace() {
+    try {
+        const result = await Promise.race([fetchTodo(), fetchUser()]);
+        console.log('First resolved promise:', result);
+    } catch (error) {
+        console.error('Error with Promise.race:', error);
+    }
+}
+fetchDataAll();
+fetchDataRace();
